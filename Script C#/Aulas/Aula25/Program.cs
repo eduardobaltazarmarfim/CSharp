84c7c284192d6f681bea92596afc9ff7e5108bb8
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2020-10-20 21:14:19 -0300

    Atualizar

---
+++ Script C#/Aulas/Aula25/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aula25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+            Console.Write("Digite um valor: ");
+            valor=int.Parse(Console.ReadLine());
+
+            Console.Write("O dobro de {0} é {1}",valor,dobrar(valor));
+        }
+
+        static int dobrar(int num){
+
+            return num*=2;
+
+        }
+    }
+}
---
+++ Script C#/Aulas/Aula26/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int divid,divis,quoc,rest;
+            divid=10;
+            divis=5;
+            quoc=divide(divid,divis,out rest);
+            Console.Write("{0}/{1}:quociente={2} e resto={3}",divid,divis,quoc,rest);
+
+
+        }
+
+        static int divide(int dividendo, int divisor, out int resto){
+
+            int quociente;
+            quociente=dividendo/divisor;
+            resto=dividendo%divisor;
+            return quociente;
+        }
+    }
+}

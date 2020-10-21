8a6daaf86c1bb23e22de59bf4b556230196b0427
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2020-10-20 22:36:42 -0300

    Atualizado

---
+++ Script C#/Aulas/Aula28/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aula28
+{
+
+    public class Jogador{
+
+        public int energia=100;
+        public bool vivo=true;
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Jogador j1=new Jogador();
+            j1.energia=150;
+            Console.Write(j1.energia);
+        }
+    }
+}

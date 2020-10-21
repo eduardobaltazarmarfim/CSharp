06ac0b12003dfbcb0e5661bec88e09e3c6dfd849
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2020-10-20 21:35:44 -0300

    Atualizar

--- Script C#/Aulas/Aula27/Program.cs
+++ Script C#/Aulas/Aula27/Program.cs
@@ -7,6 +7,28 @@ namespace Aula27
         static void Main(string[] args)
         {
             
+            soma(0,15,20);
+        }
+
+        static void soma(params int[] dados){
+
+            int res=0;
+          
+            if(dados.Length<1){
+
+                Console.Write("Não tem dados para ser calculado!");
+            
+            }else{
+
+                for(int i=0;i<dados.Length;i++){
+
+                    res+=dados[i];
+                }
+
+            }
+
+            Console.Write("O resultado Total dos dados é {0}",res);
+
         }
     }
 }

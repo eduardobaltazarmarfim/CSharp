3b5e2563add425e02ec249c9c79fa47a62d38833
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2020-10-29 22:50:49 -0300

    Atualizar

---
+++ Script C#/Atividade/Atv01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Atv01
+{
+    class Program
+    {
+
+        static public int valor1=10;
+        
+        static void Main()
+        {
+            
+            int soma,valor2;
+                        
+            valor2=5;
+            valor1=20;
+
+            soma=valor1+valor2;
+
+            Console.Write(soma);
+               
+        }
+    }
+}
---
+++ Script C#/Aulas/Aula29/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

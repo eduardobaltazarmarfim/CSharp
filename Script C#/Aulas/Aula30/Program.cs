320dafc605ddaf1b4d9a4ab0266c58cbfb6601ff
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2020-12-31 14:15:44 -0300

    a

---
+++ Script C#/Aulas/Aula30/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Aula30
+{
+
+    public class Jogador{
+
+        public string nome;
+        public int vidas;
+
+        public void info(){
+
+            Console.WriteLine("Nome do jogador {0} e quantidade de vidas {1}",nome,vidas);
+        }
+
+        public Jogador(){
+
+            nome="Eduardo";
+            vidas=10;
+        }
+
+        public Jogador(string n){
+
+            nome=n;
+            vidas=3;
+        }
+
+        public Jogador(string n,int num){
+
+            nome=n;
+            vidas=num;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Clear();
+
+            Jogador j1=new Jogador();
+            Jogador j2=new Jogador("Maira");
+            Jogador j3=new Jogador("Eduardo",100);
+                             
+            j1.info();
+            j2.info();
+            j3.info();
+  
+          }
+    }
+}

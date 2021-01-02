e7310f60c8286d75d027564ea40b10d2a8c49452
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2021-01-02 14:37:20 -0300

    Atualizar

---
+++ Script C#/Aulas/Aula40/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula40
+{
+
+    sealed class Veiculo{
+
+        public string nome;
+
+        public Veiculo(){
+
+            nome="Eduardo";
+            
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Veiculo c1=new Veiculo();
+
+            Console.WriteLine(c1.nome);
+        }
+    }
+}
---
+++ Script C#/Aulas/Aula41/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Aula41
+{
+
+    class Carro{
+
+        private int vlmax;
+
+        public int vm{
+
+            get{
+
+                return vlmax;
+
+            }set{
+
+                if(value<0){
+
+                    vlmax=0;
+                }else if(value>100){
+
+                    vlmax=100;
+                }else{
+
+                    vlmax=value;
+                }
+
+            }
+        }
+        public Carro(){
+
+            vm=150;
+
+        }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Carro c1=new Carro();
+
+            c1.vm=50;
+
+            Console.WriteLine("Velocidade {0}",c1.vm);
+        }
+    }
+}

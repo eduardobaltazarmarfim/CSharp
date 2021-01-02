a9635cbcf5094075adee91e4838424d565de3bbf
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2021-01-01 21:33:41 -0300

    a

---
+++ Script C#/Aulas/Aula34/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Aula34
+{
+    class Veiculo{
+
+        public int velmax;
+        public bool ligado;
+
+        public void ligar(){
+
+            ligado=true;
+            
+        }
+
+        public void desligar(){
+
+            ligado=false;
+
+        }
+
+    }
+
+    class Carro:Veiculo{
+
+        
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}

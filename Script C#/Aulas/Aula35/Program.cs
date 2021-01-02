52822410ec9159732a95b5d128483c3f63f45d45
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2021-01-02 11:44:04 -0300

    a

---
+++ Script C#/Aulas/Aula35/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Aula35
+{
+
+    class Veiculo{
+
+        public int vlmax;
+        private bool ligado;
+
+        private int rodas;
+
+        public Veiculo(int rodas){
+
+            this.rodas=rodas;
+         
+        }
+
+        public bool getligar(){
+
+            ligado=true;
+            return ligado;
+
+        }
+
+        public bool getdesligar(){
+
+            ligado=false;
+
+            return ligado;
+
+        }
+
+
+        public int getrodas(){
+
+            return rodas;
+
+        }
+
+    }
+
+
+    class Carro:Veiculo{
+
+        public Carro():base(4){
+
+            vlmax=120;
+            getligar();
+
+        }
+
+    }
+
+    class Carrocombate:Carro{
+
+
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Carrocombate c1=new Carrocombate();
+
+            Console.WriteLine("{0} {1} {2}",c1.getrodas(),c1.getligar(),c1.vlmax);
+        }
+    }
+}

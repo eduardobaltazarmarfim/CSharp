a95c7318ded7a8638fab598990d91af63a7ac3be
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2021-01-02 10:31:48 -0300

    a

--- Script C#/Aulas/Aula34/Program.cs
+++ Script C#/Aulas/Aula34/Program.cs
@@ -4,8 +4,9 @@ namespace Aula34
 {
     class Veiculo{
 
+        public int rodas;
         public int velmax;
-        public bool ligado;
+        private bool ligado;
 
         public void ligar(){
 
@@ -19,11 +20,27 @@ namespace Aula34
 
         }
 
+        public bool getligado(){
+
+            return ligado;
+
+        }
+
     }
 
     class Carro:Veiculo{
 
-        
+        public string nome;
+        public string cor;
+        public Carro(string nome, string cor){
+
+            desligar();
+            rodas=4;
+            velmax=120;
+            this.nome=nome;
+            this.cor=cor;
+
+        }
 
     }
 
@@ -31,7 +48,21 @@ namespace Aula34
     {
         static void Main(string[] args)
         {
-            
+
+            Console.Clear();
+
+            Carro vc =new Carro("Ferrare","Vermelho");
+
+            if(vc.getligado()){
+
+             Console.WriteLine("A carro usado é {0} a cor dele é {1} ele tem {2} rodas ele está {3}, andando à {4} Km",vc.nome,vc.cor,vc.rodas,vc.velmax); 
+
+            }else{
+
+                Console.WriteLine("Carro desligado");
+
+            }
+
         }
     }
 }

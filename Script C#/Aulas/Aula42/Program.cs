042f4bc849a168bdb8ca180f14fe381b1c3f0c49
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2021-01-02 15:34:48 -0300

    Atualizar

---
+++ Script C#/Aulas/Aula42/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Aula42
+{
+
+    class Carro{
+
+        private  int[] velmax=new int[5]{80,120,160,240,390};
+        
+        public int this[int i]{
+
+            get{
+
+                return velmax[i];
+
+            }set{
+
+                if(value<0){
+
+                    velmax[i]=0;
+                }else if(value>100){
+
+                    velmax[i]=100;
+                }else{
+
+                    velmax[i]=value;
+                }
+
+            }
+
+        }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Carro c1=new Carro();
+
+            Console.Write("Dados da lista: ");
+
+            for(int i=0;i<5;i++){
+
+                Console.Write(" {0} ",c1[i]);
+
+            }
+
+        }
+    }
+}
---
+++ Script C#/Aulas/Aula43/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Aula43
+{
+
+    public interface Veiculo{
+        
+        void ligar();
+        void desligar();
+        void info();
+    }
+
+    public interface Combate{
+
+        void disparar();
+
+    }
+
+    class Carro:Veiculo,Combate{
+
+        public bool ligado;
+        private int municao;
+        public Carro(){
+            
+
+        }
+
+        public int setmunicao{
+
+            get{
+
+                return municao;
+
+            }set{
+
+                municao=value;
+
+            }
+        }
+
+        public void ligar(){
+
+            ligado=true;
+        }
+
+        public void desligar(){
+
+            ligado=false;
+        }
+
+        public void disparar(){
+
+            
+        }
+
+        public void info(){
+
+
+        }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Carro c1=new Carro();
+
+            c1.setmunicao=20;
+
+            Console.WriteLine("Munição {0}",c1.setmunicao);
+                        
+        }
+    }
+}

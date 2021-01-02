7afdd4e11b4629944ea04f3e7eccc50f1a8fa5ac
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2021-01-02 12:54:38 -0300

    atualizar

---
+++ Script C#/Aulas/Aula37/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Aula37
+{
+
+    class Base{
+
+        public Base(){
+
+            Console.WriteLine("Construtor da Classe Base");
+
+        }
+
+    }
+
+    class Derivada:Base{
+
+        public Derivada(){
+
+            Console.WriteLine("Construtor da classe derivada");
+
+        }
+
+    }
+
+    class Derivada1:Derivada{
+
+        public Derivada1(){
+
+            Console.WriteLine("Construtor da classe derivada1");
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Derivada1 c1=new Derivada1();
+
+        }
+    }
+}
---
+++ Script C#/Aulas/Aula38/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Aula38
+{
+
+    class Base{
+
+        public Base(){
+
+            Console.WriteLine("Construtor da Classe Base");
+
+        }
+
+        virtual public void info(){
+
+            Console.WriteLine("Base");
+        }
+
+    }
+
+    class Derivada:Base{
+
+        public Derivada(){
+
+            Console.WriteLine("Construtor da classe derivada");
+
+        }
+
+        override public void info(){
+
+            Console.WriteLine("Derivada");
+
+        }        
+
+    }
+
+    class Derivada1:Derivada{
+
+        public Derivada1(){
+
+            Console.WriteLine("Construtor da classe derivada1");
+
+        }
+
+        override public void info(){
+
+            Console.WriteLine("Derivada1");
+
+        }
+
+    }    
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Base Ref;
+            Derivada1 c1=new Derivada1();
+            Derivada c2=new Derivada();
+
+            Ref=c2;
+
+            Ref.info();
+           
+        }
+    }
+}

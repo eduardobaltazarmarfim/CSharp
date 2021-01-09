f0530c2600af8a6da83d0b39799ae108711a7479
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2021-01-08 22:12:12 -0300

    Atualizar

---
+++ Script C#/Aulas/Aula49/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula49
+{
+    class Mat{
+
+        public static double pi=3.14;
+
+        public static int dobro(int n1){
+
+            return n1*2;
+
+        }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            double vp1=Mat.pi;
+            
+            Console.Write(Mat.dobro(10));
+
+        }
+    }
+}
---
+++ Script C#/Aulas/Aula50/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Aula50
+{
+
+    delegate int operacao(int n1,int n2);
+    class Mat{
+
+        public static int soma(int n1,int n2){
+
+            return n1+n2;
+
+        }
+
+        public static int mult(int n1,int n2){
+
+            return n1*n2;
+        }
+        
+    }
+
+    class Program
+    {
+        static void Main()
+        {   
+
+            int res;
+
+            operacao cp1=new operacao(Mat.soma);
+
+            res=cp1(10,15);
+
+            Console.WriteLine(res);
+
+            cp1=new operacao(Mat.mult);
+
+            res=cp1(15,20);
+
+            Console.WriteLine(res);
+        }
+    }
+}

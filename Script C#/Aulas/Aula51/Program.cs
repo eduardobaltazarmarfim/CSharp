b946787c8af8e3dc6c24d2c36e77987209740d9c
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2021-01-08 22:26:29 -0300

    Atualizar

---
+++ Script C#/Aulas/Aula51/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length>0){
+
+                Console.WriteLine(args.Length);
+                
+                for(int i=0;i<args.Length;i++){
+
+                    Console.WriteLine("{0}",args[i]);
+                }
+
+            }else{
+
+                Console.WriteLine("Sem argumentos.");
+            }
+        }
+    }
+}

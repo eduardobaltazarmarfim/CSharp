9be703a8365652005cd3c5a0964534126f5d815a
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2021-01-08 22:57:21 -0300

    Atualizar

---
+++ Script C#/Aulas/Aula52/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Aula52
+{
+
+    class Area{
+
+        public static float quadrado(float n1,float n2){
+                
+            if(n1==0||n2==0){
+
+                    throw new Exception("Base de valor invalida!");
+            }else{
+
+                return n1*n2;
+
+            }
+            
+
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+
+            float area=0;
+            
+            try{
+
+                area=Area.quadrado(0f,0f);
+                Console.WriteLine(area);
+
+            }catch(Exception erro){
+
+                Console.WriteLine("ERRO: {0}",erro.Message);
+
+            }
+            
+            
+        }
+    }
+}

b64adacec198bb0af32c9b281b926536f8fc75f2
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2021-01-13 20:38:18 -0300

    Atualizar

---
+++ Script C#/Aulas/Aula56/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula56
+{
+    class Program
+    {
+        static void Main()
+        {
+            LinkedList<string> transporte=new LinkedList<string>();
+
+            transporte.AddFirst("Carro");
+            transporte.AddFirst("Ônibus");
+            transporte.AddFirst("Trêm");
+            transporte.AddFirst("Avião");
+            transporte.AddFirst("Moto");
+
+            transporte.AddLast("Bicicleta");
+
+            LinkedListNode<string> texto;
+            
+            texto=transporte.FindLast("Trêm");
+
+            transporte.AddAfter(texto,"Patinete");
+
+            transporte.AddBefore(texto,"Skite");
+                       
+            foreach(string v in transporte){
+
+                Console.WriteLine(v);
+
+            }
+
+            Console.WriteLine(transporte.Count);
+        }
+    }
+}

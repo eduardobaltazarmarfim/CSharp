c1d5c19d3e710b8d3ff4c897fa6d7c5fb8bb3438
eduardobaltazarmarfim <66966652+eduardobaltazarmarfim@users.noreply.github.com>
2021-02-06 14:21:22 -0300

    Atualizar

---
+++ Script C#/Aulas/Aula57/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula57
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            List<string> carro=new List<string>();
+            string[] carros2=new string[10];
+
+            carro.Add("Gol");
+            carro.Add("Ferrare");
+            carro.Add("Fiat");
+            carro.Add("Gipe");
+
+            string c="Fiat";
+            int pos=0;
+
+            pos=carro.IndexOf(c);
+            Console.Write(pos);
+
+        }
+    }
+}
